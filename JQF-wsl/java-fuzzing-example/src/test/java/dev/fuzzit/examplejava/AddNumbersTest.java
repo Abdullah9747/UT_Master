f636bed851da40dc8bc0f38f22ee1866e93d531b
Abdullah9747 <abdullahfaisal9747@gmail.com>
2024-11-18 09:41:14 +0500

    Automated Test file Gen

--- JQF-wsl/java-fuzzing-example/src/test/java/dev/fuzzit/examplejava/AddNumbersTest.java
+++ JQF-wsl/java-fuzzing-example/src/test/java/dev/fuzzit/examplejava/AddNumbersTest.java
@@ -1,14 +1,13 @@
 package dev.fuzzit.examplejava;
-
 import org.junit.runner.RunWith;
 import edu.berkeley.cs.jqf.fuzz.Fuzz;
 import edu.berkeley.cs.jqf.fuzz.JQF;
 
 @RunWith(JQF.class)
-public class AddNumbersTest {
+public class AddNumbersTest{ 
 
-    @Fuzz
-    public void fuzz(int a, int b) {
-        AddNumbers.add(a, b);
-    }
+@Fuzz
+public void fuzz(int a,int b){
+      AddNumbers.add(a,b);
+}
 }
